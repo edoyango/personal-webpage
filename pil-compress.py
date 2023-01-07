bd1ea6ed024b09032ebac7f49e284a5dbd92cea0
Edward Yang <yang.e@wehi.edu.au>
2023-01-08 09:51:29 +1100

    size args

--- pil-compress.py
+++ pil-compress.py
@@ -5,8 +5,10 @@ import os
 import glob
 import sys
 
-args = sys.argv[1:]
+size_x = int(sys.argv[1])
+size_y = int(sys.argv[2])
+args = sys.argv[3:]
 for i in args:
 	img = Image.open(i)
-	img = img.resize((800,800),Image.Resampling.LANCZOS)
+	img = img.resize((size_x,size_y),Image.Resampling.LANCZOS)
 	img.save(i,optimize=True,quality=80)
