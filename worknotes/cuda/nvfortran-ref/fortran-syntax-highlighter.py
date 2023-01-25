73871aeda3ba104435dc09c9a87ac670de922fea
Edward Yang <edward_yang_125@hotmail.com>
2023-01-26 08:52:10 +1100

    mpiDevices

--- worknotes/cuda/nvfortran-ref/fortran-syntax-highlighter.py
+++ worknotes/cuda/nvfortran-ref/fortran-syntax-highlighter.py
@@ -19,6 +19,7 @@ classCuf = 'fnv'
 classPp = 'fpp'
 classIntrinsic = 'fintrinsic'
 classLogical = 'flog'
+classMPI = 'fmpi'
 openTagStd = openTag.format(c = classStd)
 openTagString = openTag.format(c = classString)
 openTagComment = openTag.format(c = classComment)
@@ -26,6 +27,7 @@ openTagCuf = openTag.format(c = classCuf)
 openTagPp = openTag.format(c = classPp)
 openTagIntrinsic = openTag.format(c = classIntrinsic)
 openTagLogical = openTag.format(c = classLogical)
+openTagMPI = openTag.format(c = classMPI)
 
 matches = re.findall(r'\"(.+?)\"',content)  # match text between two quotes
 for m in matches:
@@ -51,7 +53,6 @@ for m in matches_chevron:
     content = content.replace('<<<{}>>>'.format(m), '{}&lt;&lt;&lt;{}&gt;&gt;&gt;{}'.format(openTagCuf, m, closeTag))
 
 stdwords = ['stop' 'advance', 'deallocate', 'STAT', 'allocate', 'logical', 'allocatable', 'contains','value', 'real', 'subroutine', 'program', 'use', 'implicit', 'none', 'type', 'integer', 'if', 'then', 'else', 'elseif', 'end', 'endif', 'do', 'enddo', 'module', 'parameter', 'kind', 'endprogram', 'endmodule', 'call']
-
 for w in stdwords:
     content = re.sub(r'\b{}\b'.format(w), '{o}{w}{c}'.format(o=openTagStd, w=w, c=closeTag), content)
 
@@ -59,7 +60,7 @@ cufwords = ['shared', 'dim3', 'cuda_stream_kind', 'PINNED','cudaEvent', 'pinned'
 for w in cufwords:
     content = re.sub(r'\b{}\b'.format(w), '{o}{w}{c}'.format(o=openTagCuf, w=w, c=closeTag), content)
 
-cuffuncs = ['cudaDeviceDisablePeerAccess', 'cudaMemcpyPeer', 'cudaDeviceEnablePeerAccess', 'cudaDeviceCanAccessPeer', 'cudaMemGetInfo', 'cudaSetDevice', 'int_ptr_kind', 'syncthreads', 'cudaStreamDestroy', 'cudaStreamCreate', 'cudaMemcpyAsync', 'cudaMemcpy', 'cudaMemcpy2D', 'cudaMemcpy3D', 'cudaEventSynchronize', 'cudaEventDestroy', 'cudaEventElapsedTime', 'cudaEventRecord', 'cudaEventCreate', 'attributes','cudaGetDeviceCount', 'cudaGetDeviceProperties', 'cudaGetErrorString', 'cudaGetLastError', 'cudaDeviceSynchronize']
+cuffuncs = ['cudaGetDevice', 'cudaDeviceDisablePeerAccess', 'cudaMemcpyPeer', 'cudaDeviceEnablePeerAccess', 'cudaDeviceCanAccessPeer', 'cudaMemGetInfo', 'cudaSetDevice', 'int_ptr_kind', 'syncthreads', 'cudaStreamDestroy', 'cudaStreamCreate', 'cudaMemcpyAsync', 'cudaMemcpy', 'cudaMemcpy2D', 'cudaMemcpy3D', 'cudaEventSynchronize', 'cudaEventDestroy', 'cudaEventElapsedTime', 'cudaEventRecord', 'cudaEventCreate', 'attributes','cudaGetDeviceCount', 'cudaGetDeviceProperties', 'cudaGetErrorString', 'cudaGetLastError', 'cudaDeviceSynchronize']
 for fu in cuffuncs:
     content = re.sub(r'\b{}\('.format(fu), '{o}{fu}{c}('.format(o=openTagCuf, fu=fu, c=closeTag), content)
 
@@ -72,5 +73,9 @@ content = re.sub(r'\bwrite\(', '{o}{w}{c}('.format(o=openTagStd, w='write', c=cl
 logicalwords = ['and', 'or', 'lt', 'gt', 'not', 'eq', 'ge', 'le' ,'false', 'true']
 for w in logicalwords:
     content = re.sub(r'\.{}\.'.format(w), '{o}.{w}.{c}'.format(o=openTagLogical, w=w, c=closeTag), content)
+
+mpiwords = ['MPI_BARRIER', 'MPI_INIT', 'MPI_FINALIZE', 'MPI_COMM_WORLD', 'MPI_COMM_SIZE', 'MPI_COMM_RANK']
+for w in mpiwords:
+    content = re.sub(r'\b{}\b'.format(w), '{o}{w}{c}'.format(o=openTagMPI, w=w, c=closeTag), content)
 
 print(content)
