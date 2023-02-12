b07abf3b4d32aca26548c02427591e311e6d0fa2
Edward Yang <edward_yang_125@hotmail.com>
2023-02-12 20:51:59 +1100

    bug fix

--- worknotes/useful/samples/dsearch.F90
+++ worknotes/useful/samples/dsearch.F90
@@ -90,7 +90,7 @@ contains
          xi(:) = x(:, i)
          do j = i + 1, npoints
             r2 = sum((xi(:) - x(:, j))**2)
-            if (r2 <= cutoff) then
+            if (r2 <= cutoff**2) then
                npairs = npairs + 1
                pair_j(npairs) = j
             end if
@@ -115,7 +115,7 @@ contains
          xi(:) = x(:, i)
          do j = i + 1, npoints
             r2 = sum((xi(:) - x(:, j))**2)
-            if (r2 <= cutoff) then
+            if (r2 <= cutoff**2) then
                npairs = npairs + 1
                pair_j(npairs) = j
             end if
